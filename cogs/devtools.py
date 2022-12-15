261626a513147c64f800b9834d3d4efc8606ab48
Abe <abe@habet.dev>
2022-12-15 13:42:12 -0500

    add devtools

---
+++ cogs/devtools.py
@@ -0,0 +1,69 @@
+import logging
+import typing
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.ext.commands import Context
+
+_logger = logging.getLogger(__name__)
+
+
+class DevTools(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(
+        self,
+        ctx: commands.Context,
+        guilds: commands.Greedy[discord.Object],
+        spec: typing.Optional[typing.Literal["~", "*", "^"]] = None,
+    ) -> None:
+        """Syncs command tree.
+        Parameters
+        -----------
+        guilds: list[int]
+            The guilds to sync to
+        spec: str
+            The spec to sync.
+            ~ -> Current Guild
+            * -> Globals to current guild
+            ^ -> Clear globals copied to current guild.
+        """
+        if not guilds:
+            if spec == "~":
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "^":
+                ctx.bot.tree.clear_commands(guild=ctx.guild)
+                await ctx.bot.tree.sync(guild=ctx.guild)
+                synced = []
+            else:
+                synced = await ctx.bot.tree.sync()
+            await ctx.send(
+                f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+            )
+            return
+        ret = 0
+        for guild in guilds:
+            try:
+                await ctx.bot.tree.sync(guild=guild)
+            except discord.HTTPException:
+                pass
+            else:
+                ret += 1
+        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+
+async def setup(bot):
+    _logger.info("Loading DevTools cog")
+    await bot.add_cog(DevTools(bot))
+
+
+async def teardown(_):
+    _logger.info("Extension: Unloading DevTools")
