1df4cee7099937744bc20762046fa0a729af7ade
Abe <abe@habet.dev>
2023-11-01 23:22:11 -0400

    add stats

--- cogs/neetcode.py
+++ cogs/neetcode.py
@@ -14,18 +14,33 @@ from git.repo.base import Repo
 class Neetcode(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.logger = self.bot.logger
         self.pull_repo.start()
+        self.daily_report.start()  # Start the daily report loop
         self.bot_spam_channels = [
             1053845551315173397,  # bot-spam -> abes-server
             1053874909014675576,  # bot-playground -> neetcode
 
         ]
+        
+        self.command_usage_stats = {
+            "pulls": 0,
+            "leetcode_invoked": 0,
+        }
+
+    @tasks.loop(time=datetime.time(hour=0, minute=0, tzinfo=ZoneInfo("America/New_York")))
+    async def daily_report(self):
+        # Log the daily report
+        self.logger.info("Daily Report:")
+        self.logger.info(f"Number of repo pulls: {self.command_usage_stats['pulls']}")
+        self.logger.info(f"Number of leetcode command invocations: {self.command_usage_stats['leetcode_invoked']}")
 
     @tasks.loop(time=datetime.time(hour=22, minute=54, tzinfo=ZoneInfo("America/New_York")))
     async def pull_repo(self):
             o = self.repo.remotes.origin
             o.pull()
-            self.bot.logger.info("pulled repo on timer")
+            self.logger.info("pulled repo on timer")
+            self.command_usage_stats["pulls"] += 1  # Increment pull count
 
 
     async def cog_load(self) -> None:
@@ -35,13 +50,13 @@ class Neetcode(commands.Cog):
             self.repo = Repo.clone_from(
                 "https://github.com/neetcode-gh/leetcode.git", self.neetcode
             )
-            self.bot.logger.info("cloned repo")
+            self.logger.info("cloned repo")
             self.repo = Repo(self.neetcode)
         except git.exc.GitCommandError:
             self.repo = Repo(self.neetcode)
             o = self.repo.remotes.origin
             o.pull()
-            self.bot.logger.info("pulled repo")
+            self.logger.info("pulled repo")
 
         self.languages = [
             x.name
@@ -57,6 +72,7 @@ class Neetcode(commands.Cog):
     async def leetcode(
         self, interaction: discord.Interaction, number: int, language: str
     ):
+        self.command_usage_stats["leetcode_invoked"] += 1
         # add leading zeros to match file names
         number = "{:04d}".format(number)
         """Returns the leetcode solution"""
@@ -66,10 +82,10 @@ class Neetcode(commands.Cog):
                 f"there are no solutions for leetcode problem #{number} in {language}",
                 ephemeral=True
             )
-            self.bot.logger.info(f"{interaction.user} asked for problom #{number} in {language} but none exist")
+            self.logger.info(f"{interaction.user} asked for problom #{number} in {language} but none exist")
             return
 
-        self.bot.logger.info(f"{interaction.user} asked for problom #{number} in {language}")
+        self.logger.info(f"{interaction.user} asked for problom #{number} in {language}")
         with open(files[0]) as f:
             code = f.read()
         if interaction.channel_id in self.bot_spam_channels or interaction.channel.name.lower() == "leetcode":
@@ -89,6 +105,17 @@ class Neetcode(commands.Cog):
             for language in self.languages
             if current.lower() in language.lower()
         ]
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def stats(self, ctx):
+        """Reports the usage stats of the bot."""
+        stats_message = "Command Usage Stats:\n"
+        stats_message += f"Number of repo pulls: {self.command_usage_stats['pulls']}\n"
+        stats_message += f"Number of leetcode command invocations: {self.command_usage_stats['leetcode_invoked']}\n"
+        
+        await ctx.send(stats_message)
+
 
 
 async def setup(bot: commands.Bot):
